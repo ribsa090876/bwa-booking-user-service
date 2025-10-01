909a5c5946ea02baee5fd6c7b815fc9b3eadfde0
ribsa090876 <ribsasalehudin1704@gmail.comgit config --global user.email ribsasalehudin1704@gmail.com>
2025-10-01 17:52:13 +0700

    User Service Main

--- routes/registry.go
+++ routes/registry.go
@@ -16,7 +16,7 @@ type IRouteRegister interface{
 	Serve()
 }
 
-func NewRegistry(controller controllers.IControllerRegistry, group *gin.RouterGroup) IRouteRegister{
+func NewRouteRegistry(controller controllers.IControllerRegistry, group *gin.RouterGroup) IRouteRegister{
 	return &Registry{controller: controller, group: group}
 }
 
